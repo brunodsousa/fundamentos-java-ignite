ae0a1617395328b4eacb2d8b0511aa5d39338cc4
Bruno Sousa <80365075+brunodsousa@users.noreply.github.com>
2023-10-09 23:46:07 -0300

    Lista

---
+++ EstruturaDeDados.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+// O import precisa vir, obrigatoriamente, antes da declaração de classes
+
+public class EstruturaDeDados {
+    public static void main(String[] args) {
+        // Lista
+        List<String> nomes = new ArrayList<>();
+
+        nomes.add("Bruno");
+        nomes.add("Taiãma");
+        nomes.add("Serrinha");
+
+        System.out.println(nomes.get(0));
+
+        for (String nome : nomes) {
+            System.out.println("O nome que apareceu foi " + nome);
+        }
+
+        nomes.forEach(nome -> System.out.println("Nome: " + nome));
+
+        nomes.forEach(System.out::println);
+    }
+}
