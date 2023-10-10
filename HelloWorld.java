f5ab4cfe0f4f1b86f3861cb34fcea257323a1fe3
Bruno Sousa <80365075+brunodsousa@users.noreply.github.com>
2023-10-09 23:08:58 -0300

    Método Main

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,5 +8,21 @@
  */
 public class HelloWorld {
     // Todo conteúdo deverá ser inserido aqui dentro.
+    public static void main(String[] args) {
+        /*
+         * public = modificador de acesso que indica que o método poderá ser acessado de
+         * qualquer lugar no programa
+         * static = indica que o método main pertence a essa classe (HelloWorld) e não a
+         * uma instância específica
+         * void = tipo de retorno que o método vai ter
+         * main = nome do método; esse método precisa necessariamente chamar main, pois
+         * é definido pelo Java como o ponto inicial/de partida do programa
+         * String[] args = tipo do conteúdo que será passado como argumento para o
+         * método
+         * 
+         * Uma aplicação precisa ter, obrigatoriamente, o método main.
+         */
+        System.out.println("Hello, Java!");
+    }
 }
 // Fora do escopo da classe
