d4a7bc71be5289b7f5a1997ae7733784396c074b
Bruno Sousa <80365075+brunodsousa@users.noreply.github.com>
2023-10-09 22:59:18 -0300

    Estrutura de Classe

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/*
+ * HelloWorld = nome da classe
+ * public = tipo de acesso da classe
+ * class = tipo da classe
+ * 
+ * O nome da classe deve ser o mesmo do nome do arquivo, com extensão .java
+ * O escopo da classe começa quando abrimos chave e termina quando a fechamos
+ */
+public class HelloWorld {
+    // Todo conteúdo deverá ser inserido aqui dentro.
+}
+// Fora do escopo da classe
