9f4deec8057d273f07fad7c03f3f6e83d7fec707
Bruno Sousa <80365075+brunodsousa@users.noreply.github.com>
2023-10-16 07:39:29 -0300

    Exception: Checked exceptions

---
+++ excecoes/Excecoes.java
@@ -0,0 +1,27 @@
+package excecoes;
+
+public class Excecoes {
+
+    // Checked exceptions: aquelas que o compilador do Java exige que sejam
+    // tratadas; previstas
+
+    // Unchecked exceptions: não previstas, o compilador não consegue saber que terá
+    // o erro
+
+    public static void main(String[] args) {
+        // try / catch
+        try {
+            validarNumero();
+        } catch (Exception e) {
+            System.out.println("Deu ruim.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void validarNumero() throws Exception {
+        int numero = 10;
+        if (numero < 100) {
+            throw new Exception("O número é menor que 100");
+        }
+    }
+}
